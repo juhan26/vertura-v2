07dfcd7324c52af2bfba7c1d1cde1837fad774bc
macbook <macbook@macbooks-MacBook-Pro.local>
2025-06-27 12:37:36 +0800

    update

--- src/components/Mraquee.jsx
+++ src/components/Mraquee.jsx
@@ -8,7 +8,7 @@ const Marquee = () => {
       data-scroll
       data-scroll-section
       data-scroll-speed="1"
-      className="w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#205eab]"
+      className="w-full py-6 rounded-tl-3xl rounded-tr-3xl bg-[#205eab]"
       id='marquee'
     >
       <div className="text flex whitespace-nowrap overflow-hidden">
@@ -24,7 +24,7 @@ const Marquee = () => {
               repeat: Infinity,
               duration: 20, // Durasi lebih panjang untuk animasi yang lebih smooth
             }}
-            className="h-[36vw] pr-20 md:h-[10vw]"
+            className="h-[36vw] pr-20 md:h-[16vw]"
           />
         ))}
       </div>
